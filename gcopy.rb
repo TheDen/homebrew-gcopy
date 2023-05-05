f6fac0d865fc9e93cc015b86533b67811a80ec11
TheDen <gcopy@theden.sh>
2023-05-06 01:13:36 +1000

    Brew formula update for gcopy version v1.7.7

--- gcopy.rb
+++ gcopy.rb
@@ -5,12 +5,12 @@
 class Gcopy < Formula
   desc ""
   homepage "https://github.com/TheDen/gcopy"
-  version "1.7.6"
+  version "1.7.7"
   depends_on :macos
 
   on_macos do
-    url "https://github.com/TheDen/gcopy/releases/download/v1.7.6/gcopy_1.7.6_darwin_all.tar.gz"
-    sha256 "1a762ca5a80374f9ac7c6485444bd9266b0416b4620e5a36239777b99b99773c"
+    url "https://github.com/TheDen/gcopy/releases/download/v1.7.7/gcopy_1.7.7_darwin_all.tar.gz"
+    sha256 "09e77dfbcb989cd64f3a9c155c2896370cbc273a80d7208ff29379e78310f11b"
 
     def install
       bin.install "gcopy"
